ea3c56950c13325f1b8ad658614349cf48f15bfe
NicolasMK0 <nmendezvargas.05@gmail.com>
2025-05-29 15:06:30 -0300

    ahora debe funcionar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
