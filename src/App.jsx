4ff0d93283bbd9ac3c228a51123f2bbb2c2e8c39
NicolasMK0 <nmendezvargas.05@gmail.com>
2025-05-29 15:41:12 -0300

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 function App() {
   return (
     <div>
-      <Router>
-        <NavBar/>
+      <BrowserRouter basename="/Proy_Pre_React_NSM">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
-};
+}
+
 
 export default App
